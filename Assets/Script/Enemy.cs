827d98408960a4fca50b38f0f1ee5cbf69ed2ff2
ThIbNI <filip2410@yandex.ru>
2022-09-08 15:07:54 +0200

    Создание противника

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+    private Player player;
+    private Vector3 DistanceToPlayer;
+    private Vector3 CoordsEnemy;
+    private float MaxDistance;
+    private float MinDistance;
+    private float ActualDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //MaxDistance = new Vector3(20.0f, 0.0f, 20.0f);
+        MaxDistance = 20f;
+        MinDistance = 5f;
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DistanceToPlayer = player.transform.position;
+        CoordsEnemy = transform.position;
+        ActualDistance = Mathf.Abs(DistanceToPlayer.x) - Mathf.Abs(CoordsEnemy.x);
+        ActualDistance = Mathf.Abs(ActualDistance) + (Mathf.Abs(Mathf.Abs(DistanceToPlayer.z) - Mathf.Abs(CoordsEnemy.z)));
+        if (ActualDistance < MaxDistance)
+        {
+            if(ActualDistance < MinDistance)
+            {
+                navMeshAgent.SetDestination(CoordsEnemy);
+            }
+            else
+            {
+                navMeshAgent.SetDestination(DistanceToPlayer);
+            }
+        }       
+    }
+}
